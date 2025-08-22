0c238179db5d5f53bce9b7cc6180364026447889
Geetansh <geetansh.bhardwaj@virtuos.com>
2025-08-22 18:02:39 +0530

    basic scaffolding done

---
+++ ml_engine/test.py
@@ -0,0 +1,13 @@
+from ml_utilities import summarize_text
+
+print(summarize_text(""" Crayon Shin-chan (Japanese: クレヨンしんちゃん, Hepburn: Kureyon Shin-chan) is a Japanese manga series written and illustrated by Yoshito Usui. Crayon Shin-chan made its first appearance in 1990 in a Japanese weekly magazine called Weekly Manga Action, which was published by Futabasha. Due to the death of author Yoshito Usui, the manga in its original form ended on September 11, 2009. A new manga began in the summer of 2010 by members of Usui's team,[6] titled New Crayon Shin-chan (新クレヨンしんちゃん, Shin Kureyon Shin-chan).
+
+An animated television adaptation began airing on TV Asahi in 1992 and is still ongoing, with over 1200 episodes. The show has been dubbed in 30 languages which aired in 45 countries.[7] As of 2023, both the Crayon Shin-Chan and New Crayon Shin-Chan series has over 148 million copies in circulation, making it among the best-selling manga series in history.
+
+Synopsis
+Main article: List of Crayon Shin-chan characters
+
+Train in special Crayon Shin-chan vinyl wrapping livery at Kurihashi Station, Japan
+Set in the city of Kasukabe of Saitama Prefecture within the Greater Tokyo Area of Japan, the series follows the adventures of the five-year-old Shinnosuke "Shin" Nohara and his parents, baby sister, dog, neighbours, and best friends. Most of the plot is about Shin-chan's daily life, but it is also often interspersed with a lot of fantastic and incredible elements.
+
+Many of the jokes in the series stem from Shin-chan's occasionally weird, unnatural and inappropriate use of language, as well as from his mischievous behaviour. """))

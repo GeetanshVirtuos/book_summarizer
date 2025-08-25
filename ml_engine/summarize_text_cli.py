00519026b1078d1f45c43c7805130c54d7e1e7b4
Geetansh <geetansh.bhardwaj@virtuos.com>
2025-08-25 12:15:02 +0530

    basic backend initiated

--- ml_engine/summarize_text_cli.py
+++ ml_engine/summarize_text_cli.py
@@ -1,8 +1,16 @@
-import sys
+import argparse
 from ml_utilities import summarize_text
 
+# print("I am in summarize_text_cli")
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--text", required=True, help="Text to summarize")
+    args = ap.parse_args()
+
+    # Run the summarize_text() function
+    summary = summarize_text(args.text)
+    print(summary)  # print summary to stdout where it would be picked up by the "summarize_text.js" nodejs process
+
 if __name__ == "__main__":
-    # Read input text from stdin
-    input_text = sys.stdin.read()
-    summary = summarize_text(input_text)
-    print(summary)
+    main()
